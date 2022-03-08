41424600ba4f2a9892d696392ce9d72ab34590c9
elena <ikonnikovaev@gmail.com>
2022-03-08 10:10:49 +0300

    Stage 3: plots

--- A B Test for Delivery App/task/aa_test.py
+++ A B Test for Delivery App/task/aa_test.py
@@ -1,5 +1,7 @@
 # write your code here
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import scipy.stats as st
 from statsmodels.stats.power import TTestIndPower
 
@@ -43,9 +45,38 @@ print(f"Means are equal: {equal}")
 '''
 analysis = TTestIndPower()
 result = analysis.solve_power(effect_size=0.2, nobs1=None, alpha=0.05, power=0.8, ratio=1.0)
-print(f"Sample Size: {int(round(result, -2))}")
+# print(f"Sample Size: {int(round(result, -2))}")
 data_ab = pd.read_csv('ab_test.csv')
-#print(data_ab.columns)
+# print(data_ab.columns)
 vc = data_ab['group'].value_counts()
-print(f"Control group: {vc['Control']}")
-print(f"Experimental group: {vc['Experimental']}")
+#print(f"Control group: {vc['Control']}")
+#print(f"Experimental group: {vc['Experimental']}")
+
+data_ab.date = pd.to_datetime(data_ab.date).dt.day
+groups_by_date = pd.DataFrame({'Control': data_ab[data_ab.group == 'Control'].date.value_counts(),
+                    'Experimental': data_ab[data_ab.group == 'Experimental'].date.value_counts()})
+#print(groups_by_date)
+
+groups_by_date.plot.bar()
+plt.ylabel("Number of sessions")
+plt.xlabel("June")
+plt.legend(["Control", "Experimental"])
+plt.savefig("1.jpg")
+
+data_ab.order_value.hist(by=data_ab.group)
+plt.savefig("2.jpg")
+data_ab.session_duration.hist(by=data_ab.group)
+plt.savefig("3.jpg")
+
+ninety_ninth_percentile_order_value = np.percentile(data_ab.order_value, 99)
+ninety_ninth_percentile_session = np.percentile(data_ab.session_duration, 99)
+
+order_value_outliers = data_ab.loc[data_ab.order_value >= ninety_ninth_percentile_order_value].index
+session_value_outliers = data_ab.loc[data_ab.session_duration >= ninety_ninth_percentile_session].index
+
+data_no_outliers = data_ab.drop(index=order_value_outliers.union(session_value_outliers))
+#print(data_no_outliers.columns)
+print(f'Mean: {round(data_no_outliers.order_value.mean(), 2)}')
+print(f'Standard deviation: {round(data_no_outliers.order_value.std(ddof=0), 2)}')
+print(f'Max: {round(data_no_outliers.order_value.max(), 2)}')
+
